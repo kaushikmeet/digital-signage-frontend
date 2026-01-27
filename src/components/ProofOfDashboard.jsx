77703aa0b27afd5c15cbe112c54b30e5fc996ec1
Dew <maidoradewharfan@gmail.com>
2026-01-27 20:58:05 +0530

    add new screenPreview.jsx, Line chart, daily trend, KpiCard, live update real time, scoket update

--- src/components/ProofOfDashboard.jsx
+++ src/components/ProofOfDashboard.jsx
@@ -1,26 +1,56 @@
 import { useEffect, useState } from "react";
 import api from "../api/services";
-import KpiCard from "./KPICards";
+import KpiCard from "../components/analytics/KPICards";
+import DailyTrendChart from "./analytics/DailyTrends";
+import socket from "../utils/socket";
 
 export default function ProofPlayDashboard() {
   const [kpis, setKpis] = useState({});
   const [daily, setDaily] = useState([]);
+  const [stats, setStats] = useState({
+    totalPlays: 0,
+    totalDuration: 0
+  });
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     api.get("/analytics/kpis").then(res => setKpis(res.data));
     api.get("/analytics/daily").then(res => setDaily(res.data));
   }, []);
 
+  const loadStats = async () => {
+    const res = await api.get("/analytics/kpis");
+    setStats(res.data);
+  };
+
+  useEffect(() => {
+    loadStats(); // initial load
+
+    socket.on("analytics-update", () => {
+      loadStats(); // 🔴 refresh on live event
+    });
+
+    return () => socket.off("analytics-update");
+  }, []);
+
+   const exportCsv = () => {
+    const res = window.open(`http://localhost:8080/analytics/export/csv?token=${token}`, "_blank");
+    console.log(res);
+  };
+
   return (
     <div className="p-6 space-y-6">
-      <h1 className="text-2xl font-bold">📊 Proof of Play Analytics</h1>
+      <div className="flex justify-between item-center mb-4">
+          <h1 className="text-2xl font-bold">Proof of Play Analytics</h1>
+          <button onClick={exportCsv} className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700">
+            Export CSV
+          </button>
+      </div>
+     
 
       <div className="grid grid-cols-3 gap-4">
-        <KpiCard label="Total Plays" value={kpis.totalPlays} />
-        <KpiCard
-          label="Total Play Time (min)"
-          value={Math.floor((kpis.totalDuration || 0) / 60)}
-        />
+        <KpiCard label="Total Plays" value={stats.totalPlays} />
+        <KpiCard label="Total Duration" value={stats.totalDuration} />
         <KpiCard label="Avg Duration (sec)" value={
           kpis.totalPlays
             ? Math.floor(kpis.totalDuration / kpis.totalPlays)
@@ -31,11 +61,7 @@ export default function ProofPlayDashboard() {
       <div className="bg-white p-4 rounded shadow">
         <h2 className="font-semibold mb-2">Daily Plays</h2>
         <ul className="space-y-1">
-          {daily.map(d => (
-            <li key={d._id.day}>
-              {d._id.day} — {d.plays} plays
-            </li>
-          ))}
+          <DailyTrendChart data={daily}/>
         </ul>
       </div>
     </div>
